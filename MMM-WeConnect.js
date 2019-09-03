e938a73a48900fc82e3a3f99cf2fc3efd2af79e5
Otto Paulsen <otto.paulsen@itema.no>
2019-09-03 22:26:25 +0200

    Make drawing work

--- MMM-WeConnect.js
+++ MMM-WeConnect.js
@@ -32,7 +32,7 @@ Module.register("MMM-WeConnect", {
             if (payload != null) {
                 self.carData = JSON.parse(payload).reduce((m, [key, val]) => m.set(key, val), new Map());
                 console.log('Parsed carData: ', self.carData);
-                self.updateCar();
+                self.updateCar(self.carDrawing, self.carData);
             } else {
                 console.log(self.name + ': WECONNECT_CARDATA - No payload');
             }
@@ -85,8 +85,19 @@ Module.register("MMM-WeConnect", {
         let leftWheel  = svg.rect(40, 60).move(130, 200).radius(10).fill("white");
         let rightWheel = svg.rect(40, 60).move(320, 200).radius(10).fill("white");
         let cableConnected = svg.path("M100 160 h-20 a15 15 0 0 1 -15 -15 v-60 a15 15 0 0 0 -15 -15 h-50")
-            .stroke({width: 8, color: 'white'});
-    
+            .stroke({width: 8, color: 'white'}).hide();
+        let cableDisConnected = svg.path("M0 70 h20 a15 15 0 0 1 15 15 v180 m-5 15 v-10 a5 5 0 0 1 10 0 v10")
+            .stroke({width: 8, color: 'white'}).hide();
+        let chargerBox = svg.rect(15, 50).move(0, 55). radius(2).fill("white");
+        let wall = svg.line(0, 0, 0, 300).stroke({ width: 1, color: "white" });
+        let charging = svg.polygon("60,65 90,25 100,35 130,10 100,55 90,45")
+            .fill("white").rotate(-30).hide();
+        let distance = svg.text("").move(245, 230).font({size: 22, anchor: "middle", fill: "white"});
+        let battery  = svg.rect(150, 64).move(170, 160).radius(10).fill("gray").hide();
+        let batteryTop  = svg.rect(12, 34).move(320, 175).radius(3).fill("gray").hide();
+        let batteryLevel = battery.clone();
+        let batteryPercent = svg.text("").move(245, 155).font({size: 40, anchor: "middle", fill: "black"});
+        let batteryDistance = svg.text("").move(245, 110).font({size: 32, anchor: "middle", fill: "black", weight: 600});
 
         return {
             svg,
@@ -95,12 +106,66 @@ Module.register("MMM-WeConnect", {
             leftLight,
             rightLight,
             leftWheel,
-            rightWheel
+            rightWheel,
+            cableConnected,
+            cableDisConnected,
+            chargerBox,
+            wall,
+            charging,
+            distance,
+            battery,
+            batteryTop,
+            batteryLevel,
+            batteryPercent,
+            batteryDistance
         }
     },
 
-    updateCar: function () {
-        // this.carDrawing.text.text("Updated");
-        // this.carDrawing.svg.size('50%', '50%');
+    updateCar: function (drawing, carData) {
+        console.log('carData: ', carData);
+
+        // Distance covered
+        if(carData.has("distanceCovered")) {
+            drawing.distance.text(carData.get("distanceCovered").value + " " + carData.get("distanceCovered").suffix);
+        }
+
+        // Connection state
+        if(carData.has("connectionState")) {
+            if(carData.get("connectionState").value == "CONNECTED") {
+                drawing.cableConnected.show();
+                drawing.cableDisConnected.hide();
+            } else {
+                drawing.cableConnected.hide();
+                drawing.cableDisConnected.show();
+            }
+        }
+
+        // Charging
+        if(carData.has("connectionState")) {
+            if(carData.get("connectionState").value == "ON") {
+                drawing.charging.show();
+            } else {
+                drawing.charging.hide();
+            }
+        }
+
+        // Battery level
+        if(carData.has("battery")) {
+            let level = carData.get("battery").value;
+            drawing.battery.show();
+            drawing.batteryTop.show();
+            drawing.batteryLevel.width(drawing.battery.width() * level / 100).show();
+            let color = "green";
+            if(level < 20) color = "red";
+            else if(level < 50) color = "yellow";
+            else if(level < 75) color = "blue";
+            else color = "green";
+            drawing.batteryLevel.fill(color);
+
+            drawing.batteryPercent.text("" + level + " " + carData.get("battery").suffix);
+            drawing.batteryDistance.text("" + carData.get("range").value + " " + carData.get("range").suffix);
+        }
+
+
     }
 });
--- node_helper.js
+++ node_helper.js
@@ -11,86 +11,89 @@ module.exports = NodeHelper.create({
         // ]},
         // {intervalSeconds: 10, path: '/-/vsr/request-vsr', values: [
         // ]},
-        {intervalSeconds: 10, path: '/-/vsr/get-vsr', values: [
-            {sourceKey: '/vehicleStatusData/totalRange', key: 'range', label: 'Range', suffix: 'km'},
-            {sourceKey: '/vehicleStatusData/batteryLevel', key: 'battery', label: 'Battery level', suffix: '%'},
-        ]},
+        {
+            intervalSeconds: 120, path: '/-/vsr/get-vsr', values: [
+                { sourceKey: '/vehicleStatusData/totalRange', key: 'range', label: 'Range', suffix: 'km' },
+                { sourceKey: '/vehicleStatusData/batteryLevel', key: 'battery', label: 'Battery level', suffix: '%' },
+            ]
+        },
         // {intervalSeconds: 10, path: '/-/cf/get-location', values: [
         // ]},
-        {intervalSeconds: 60, path: '/-/vehicle-info/get-vehicle-details', values: [
-            {sourceKey: '/vehicleDetails/lastConnectionTimeStamp/0', key: 'lastConectionDate', label: 'Last connection date', suffix: ''},
-            {sourceKey: '/vehicleDetails/lastConnectionTimeStamp/1', key: 'lastConectionTime', label: 'Last connection time', suffix: ''},
-            {sourceKey: '/vehicleDetails/distanceCovered', key: 'distanceCovered', label: 'Distance', suffix: 'km'},
-            {sourceKey: '/vehicleDetails/serviceInspectionData', key: 'service', label: 'Service in', suffix: ''},
-        ]},
-        {intervalSeconds: 30, path: '/-/emanager/get-emanager', values: [
-            {sourceKey: '/EManager/rbc/status/chargingState', key: 'charging', label: 'Charging', suffix: ''},
-            {sourceKey: '/EManager/rbc/status/chargingRemaningHour', key: 'chargingRemainingHour', label: 'Remaining hours', suffix: 'h'},
-            {sourceKey: '/EManager/rbc/status/chargingRemaningMinute', key: 'chargingRemainingMinute', label: 'Remaining minutes', suffix: '0'},
-            {sourceKey: '/EManager/rbc/status/pluginState', key: 'connectionState', label: 'Connection state', suffix: ''},
-        ]}
+        {
+            intervalSeconds: 300, path: '/-/vehicle-info/get-vehicle-details', values: [
+                { sourceKey: '/vehicleDetails/lastConnectionTimeStamp/0', key: 'lastConectionDate', label: 'Last connection date', suffix: '' },
+                { sourceKey: '/vehicleDetails/lastConnectionTimeStamp/1', key: 'lastConectionTime', label: 'Last connection time', suffix: '' },
+                { sourceKey: '/vehicleDetails/distanceCovered', key: 'distanceCovered', label: 'Distance', suffix: 'km' },
+                { sourceKey: '/vehicleDetails/serviceInspectionData', key: 'service', label: 'Service in', suffix: '' },
+            ]
+        },
+        {
+            intervalSeconds: 60, path: '/-/emanager/get-emanager', values: [
+                { sourceKey: '/EManager/rbc/status/chargingState', key: 'charging', label: 'Charging', suffix: '' },
+                { sourceKey: '/EManager/rbc/status/chargingRemaningHour', key: 'chargingRemainingHour', label: 'Remaining hours', suffix: 'h' },
+                { sourceKey: '/EManager/rbc/status/chargingRemaningMinute', key: 'chargingRemainingMinute', label: 'Remaining minutes', suffix: '0' },
+                { sourceKey: '/EManager/rbc/status/pluginState', key: 'connectionState', label: 'Connection state', suffix: '' },
+            ]
+        }
     ],
 
     start: function () {
         console.log(this.name + ': Starting node helper');
         this.loaded = false;
     },
-    
-    startReadingData: function () {        
+
+    startReadingData: function () {
         this.resources.forEach(resource => {
             this.readCarData(resource);
         });
     },
 
-    loginWeConnect: async function (config) {
-        console.log(this.name + ': Logging in to WeConnect');
-        // const self = this;
+    loginWeConnect: async function (config, retries = 1) {
+        console.log(this.name + ': Logging in to WeConnect (' + retries + ')');
+        let self = this;
         weconnect.login(config.email, config.password)
-        .then(res => {
-            console.log(this.name + ': Logged in to WeConnect');
-            this.loaded = true;
-            this.startReadingData();
-        })
-        .catch(err => {
-            console.log(this.name + ': Error logging in weconnect: ', err);
-        });
+            .then(res => {
+                console.log(this.name + ': Logged in to WeConnect');
+                this.loaded = true;
+                this.startReadingData();
+            })
+            .catch(err => {
+                console.log(this.name + ': Error logging in weconnect: ', err);
+                if (retries <= 2) {
+                    console.log(this.name + 'Retrying');
+                    self.loginWeConnect(config, retries + 1);
+                }
+            });
     },
 
-    readCarData: function (resource){
+    readCarData: function (resource) {
         console.log(this.name + ': Reading car data for ' + resource.path);
 
         weconnect.api(resource.path)
-        .then(data => {
-            console.log('Got data: ', data);
-            resource.values.forEach((value, key) => {
-                res = jsonpointer.get(JSON.parse(data), value.sourceKey);
-                carData.set(value.key, {
-                    label: value.label,
-                    value: res,
-                    suffix: value.suffix
-                });
+            .then(data => {
+                console.log('Got data: ', data);
+                resource.values.forEach((value, key) => {
+                    res = jsonpointer.get(JSON.parse(data), value.sourceKey);
+                    carData.set(value.key, {
+                        label: value.label,
+                        value: res,
+                        suffix: value.suffix
+                    });
+                })
+                console.log('Sending data: ', carData);
+                this.sendSocketNotification('WECONNECT_CARDATA', JSON.stringify([...carData.entries()]));
+                console.log('Waiting ' + resource.intervalSeconds + ' seconds to call again');
+                setTimeout(() => {
+                    this.readCarData(resource);
+                }, 15000);
             })
-            console.log('Sending data: ', carData);
-            this.sendSocketNotification('WECONNECT_CARDATA', JSON.stringify([...carData.entries()]));
-            console.log('Waiting ' + resource.intervalSeconds + ' seconds to call again');
-            setTimeout(() => {
-                this.readCarData(resource);
-            }, 15000);
-        })
-        .catch(err => {
-            console.log('Error reading car data: ', err);
-            setTimeout(() => {
-                this.readCarData(resource);
-            }, 15000);
-            console.log('Trying again in ' +  + 'seconds to call again');
-        })
-
-
-        // self.sendSocketNotification('WECONNECT_CARDATA', [{
-        //     label: 'Batteri',
-        //     value: '82',
-        //     suffix: '%'
-        // }]);
+            .catch(err => {
+                console.log('Error reading car data: ', err);
+                setTimeout(() => {
+                    this.readCarData(resource);
+                }, 15000);
+                console.log('Trying again in ' + + 'seconds to call again');
+            })
     },
 
 
